7fa06d2453cbb4c501799b6bc7904a3bc513017f
Braz <enzo.braz@germinare.org.br>
2025-08-27 10:35:34 -0300

    criando js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+toggleButton = document.querySelector('.toggle-button')
+sidebar = document.querySelector('.sidebar')
+closeButton = document.querySelector('.close-button')
+
+toggleButton.addEventListener('click', function(){
+    sidebar.classList.toggle('show-sidebar')
+})
+
+closeButton.addEventListener('click', function(){
+    sidebar.classList.remove('show-sidebar')
+})
